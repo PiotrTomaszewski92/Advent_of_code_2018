ae60651c3d70b34842ed952c2db41741d1d4c6f4
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2018-12-14 14:43:00 +0100

    implemented method to read file and solve problems from 1st day

--- src/Day1.java
+++ src/Day1.java
@@ -1,6 +1,26 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day1 {
 
+    private int countNumbers(List<String> numbers){
+
+        int result = 0;
+            for (String number : numbers){
+                result += (Integer.parseInt(number));
+            }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Day1 day1 = new Day1();
+        int solve = 0;
+        try {
+            solve = day1.countNumbers(ReadFile.readFile("input_files/day1.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(solve);
     }
 }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,2 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadFile {
+
+    public static List<String> readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        List<String> stringList = new ArrayList<>();
+        String st;
+        while ((st = br.readLine()) != null)
+            stringList.add(st);
+        return stringList;
+    }
+
 }
